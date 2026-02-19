a8cc84717c40655d8665fc12e0e4df56b1014ba8
Deployment <deployment@localhost>
2026-02-19 09:54:58 +0530

    Fix NewsSourceSeeder to use RSS fields for aggregator

--- database/seeders/NewsSourceSeeder.php
+++ database/seeders/NewsSourceSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\NewsSource;
+use App\Services\NewsAggregatorService;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,68 +14,25 @@ class NewsSourceSeeder extends Seeder
      */
     public function run(): void
     {
-        $sources = [
-            [
-                'name' => 'BBC News',
-                'url' => 'https://www.bbc.com/news',
-                'api_endpoint' => 'https://newsapi.org/v2/everything?sources=bbc-news',
-                'api_key' => env('NEWS_API_KEY', ''),
-                'source_type' => 'rss',
-                'fetch_interval' => 30,
-                'priority' => 5,
-                'is_active' => 1,
-                'last_fetched_at' => null,
-            ],
-            [
-                'name' => 'Reuters',
-                'url' => 'https://www.reuters.com',
-                'api_endpoint' => 'https://newsapi.org/v2/everything?sources=reuters',
-                'api_key' => env('NEWS_API_KEY', ''),
-                'source_type' => 'rss',
-                'fetch_interval' => 30,
-                'priority' => 5,
-                'is_active' => 1,
-                'last_fetched_at' => null,
-            ],
-            [
-                'name' => 'CNN',
-                'url' => 'https://www.cnn.com',
-                'api_endpoint' => 'https://newsapi.org/v2/everything?sources=cnn',
-                'api_key' => env('NEWS_API_KEY', ''),
-                'source_type' => 'rss',
-                'fetch_interval' => 30,
-                'priority' => 4,
-                'is_active' => 1,
-                'last_fetched_at' => null,
-            ],
-            [
-                'name' => 'The Guardian',
-                'url' => 'https://www.theguardian.com',
-                'api_endpoint' => 'https://newsapi.org/v2/everything?sources=the-guardian-uk',
-                'api_key' => env('NEWS_API_KEY', ''),
-                'source_type' => 'rss',
-                'fetch_interval' => 45,
-                'priority' => 4,
-                'is_active' => 1,
-                'last_fetched_at' => null,
-            ],
-            [
-                'name' => 'Associated Press',
-                'url' => 'https://apnews.com',
-                'api_endpoint' => 'https://newsapi.org/v2/everything?sources=associated-press',
-                'api_key' => env('NEWS_API_KEY', ''),
-                'source_type' => 'rss',
-                'fetch_interval' => 30,
-                'priority' => 5,
-                'is_active' => 1,
-                'last_fetched_at' => null,
-            ],
-        ];
+        $sources = NewsAggregatorService::getDefaultSources();
 
         foreach ($sources as $source) {
-            NewsSource::firstOrCreate(
-                ['name' => $source['name']],
-                $source
+            NewsSource::updateOrCreate(
+                ['slug' => $source['slug']],
+                [
+                    'name' => $source['name'],
+                    'website_url' => $source['website_url'],
+                    'rss_feed_url' => $source['rss_feed_url'],
+                    'api_type' => $source['api_type'],
+                    'api_key' => $source['api_key'] ?? null,
+                    'category_mapping' => $source['category_mapping'],
+                    'country' => $source['country'],
+                    'language' => $source['language'],
+                    'priority' => $source['priority'],
+                    'credibility_score' => $source['credibility_score'],
+                    'fetch_interval' => $source['fetch_interval'] ?? 30,
+                    'is_active' => true,
+                ]
             );
         }
     }
